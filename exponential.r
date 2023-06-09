717e3993966ca5b10e573e885efc91d7cb3e4dac
jotyot <104177975+jotyot@users.noreply.github.com>
2023-06-08 19:51:36 -0700

    exponential plot done

--- exponential.r
+++ exponential.r
@@ -1,1 +1,33 @@
-# weatherTS$PRECTOT
+# Replace directory with path to the Data folder
+setwd("C:/Users/jonas/Desktop/Class/ECS132/ECS132TermProject/Data")
+load("qeML/weatherTS.RData",verbose=T) 
+
+
+# Save to the local variable
+prec <- weatherTS$PRECTOT
+
+# Actual Plot
+hist(prec, probability=TRUE)
+#plot(density(prec), col = "black", xlab = "x", ylab = "y", main = "Title")
+
+
+# Average # 1/l
+A <- mean(prec)
+# lambda
+lambda <- 1 / A
+
+curve(dexp(x, lambda), 0, 60, add = TRUE, col = "red")
+
+nLL <- function(lambda)
+{
+  loglik <- sum(dexp(prec, rate = lambda, log = TRUE))
+  return(-loglik)
+}
+
+z <- stats4::mle(minuslog=nLL, start = list(lambda = .1))
+zLambda <- z@coef["lambda"]
+curve(dexp(x, zLambda), 0, 60, add = TRUE, col = "green")
+
+legend_labels <- c("Histogram", "MM", "MLE")
+legend_colors <- c("black", "red", "green")
+legend("topright", legend = legend_labels, col = legend_colors, lty = 1)
