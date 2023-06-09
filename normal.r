f04d56f9502f72236481a32df41fdeecdc882342
jotyot <104177975+jotyot@users.noreply.github.com>
2023-06-08 20:00:48 -0700

    normal graphs done

--- normal.r
+++ normal.r
@@ -1,1 +1,35 @@
-# plot(density(national.longitudinal.survey$weight))
+# Replace directory with path to the Data folder
+setwd("C:/Users/jonas/Desktop/Class/ECS132/ECS132TermProject/Data")
+load("fairml/national.longitudinal.survey.rda", verbose=T)
+
+# Save to the local variable
+wt <- national.longitudinal.survey$weight
+
+# Actual Plot
+# hist(lawschoolbrief$GPA, probability=TRUE)
+plot(density(wt), col = "black", xlab = "x", ylab = "y", main = "Title")
+
+# Average
+A <- mean(wt)
+# Variance
+S2 <- var(wt)
+
+sd <- sqrt(S2)
+
+curve(dnorm(x, A, sd), 0, 325, add = TRUE, col = "red")
+
+
+nLL <- function(m, s)
+{
+  loglik <- sum(dnorm(wt, m, s, log = TRUE))
+  return(-loglik)
+}
+
+z <- stats4::mle(minuslog=nLL, start = list(m = .1, s = .1))
+zA <- z@coef["m"]
+zSd <- z@coef["s"]
+curve(dnorm(x, zA, zSd), 0, 325, add = TRUE, col = "green")
+
+legend_labels <- c("Original", "MM", "MLE")
+legend_colors <- c("black", "red", "green")
+legend("topright", legend = legend_labels, col = legend_colors, lty = 1)
