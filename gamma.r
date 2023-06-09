9a24c3de722b0fba85569b2f1e701207e857f6bf
jotyot <104177975+jotyot@users.noreply.github.com>
2023-06-08 20:08:58 -0700

    gamma looks nicer

--- gamma.r
+++ gamma.r
@@ -1,37 +1,33 @@
 # Replace directory with path to the Data folder
-# setwd("C:/Users/jonas/Desktop/Class/ECS132/ECS132TermProject/Data")
-setwd("/Users/lloydretro2/Desktop/ECS132/TermProject/Data/")
+setwd("C:/Users/jonas/Desktop/Class/ECS132/ECS132TermProject/Data")
+#setwd("/Users/lloydretro2/Desktop/ECS132/TermProject/Data/")
 load("EDFfair/AdultFinal.RData",verbose=T) 
 
+age <- adult$age
+
 #Actual Plot
-hist(adult$age, probability=TRUE)
-plot(density(adult$age))
+#hist(age, probability=TRUE)
+plot(density(age), col = "black", ylim = c(0,.033), xlab = "x", ylab = "y", main = "Title")
 
 x <- 1:100 # Graphing ages 1-100
 
 # Method of Moments
-A <- mean(adult$age)
-S2 <- var(adult$age)
+A <- mean(age)
+S2 <- var(age)
 
 L <- A / S2 #lamb
 C <- L * A  #r 
 
-plot(dgamma(x, C, L))
-lines(density(adult$age)) # How to plot two lines on the same graph
+curve(dgamma(x, C, L), 0, 100, add = TRUE, col = "red")
 
 #Maximum Likelihood
-logbar <- mean(log(adult$age))
-n <- length(adult$age)
-
 nLL <- function(C, L)
 {
-  theta <- 1 / L
-  # from wikipedia
-  loglik <- n * (C - 1) * logbar - n * log(gamma(C)) - n * C * log(theta) - n * A / theta
+  loglik <- sum(dgamma(age, C, L, log = TRUE))
   return(-loglik)
 }
 
 z <- stats4::mle(minuslog=nLL, start = list(C = 1, L = .1))
 zC <- z@coef["C"]
 zL <- z@coef["L"]
-plot(dgamma(x, zC, zL))
+curve(dgamma(x, zC, zL), 0, 100, add = TRUE, col = "green")
