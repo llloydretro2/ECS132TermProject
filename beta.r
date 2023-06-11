4ddfd3dc1393f75682ec899678e188d72cde5969
jotyot <104177975+jotyot@users.noreply.github.com>
2023-06-11 15:42:59 -0700

    S to s

--- beta.r
+++ beta.r
@@ -2,35 +2,37 @@
 setwd("C:/Users/jonas/Desktop/Class/ECS132/ECS132TermProject/Data")
 load("EDFfair/lawschoolbrief.RData",verbose=T)
 
-# Actual Plot
-# hist(lawschoolbrief$GPA, probability=TRUE)
-plot(density(lawschoolbrief$GPA), col = "black", xlab = "x", ylab = "y", main = "Title")
-
 # Save to the local variable
 gpa <- lawschoolbrief$GPA
 
+fac <- 4.3
+
+# Actual Plot
+#hist(gpa, probability=TRUE, xlab = "GPA", main = "Histogram of GPA")
+plot(density(gpa), col = "black", xlab = "GPA", ylab = "Density", main = "Density of GPA w/ Approximation (0, 4.3)", xlim = c(0,fac))
+
 # Average
-A <- mean(gpa) / 5
+A <- mean(gpa) / fac
 # Variance
-S2 <- var(gpa) / 25
+S2 <- var(gpa) / fac^2
 # alpha
 alpha <- A * ((A * (1 - A) / S2) - 1)
 # beta
 beta <- alpha / A - alpha
 
-curve(dbeta(x/5, alpha, beta)/5, 0, 5, add = TRUE, col = "red")
+curve(dbeta(x/fac, alpha, beta)/fac, 0, fac, add = TRUE, col = "red")
 
 
 nLL <- function(alpha, beta)
 {
-  loglik <-sum(dbeta(gpa/5, shape1 = alpha, shape2 = beta, log = TRUE))
+  loglik <-sum(dbeta(gpa/fac, shape1 = alpha, shape2 = beta, log = TRUE))
   return(-loglik)
 }
 
 z <- stats4::mle(minuslog=nLL, start = list(alpha = .1, beta = .1))
 zAlpha <- z@coef["alpha"]
 zBeta <- z@coef["beta"]
-curve(dbeta(x/5, zAlpha, zBeta) / 5, 0, 5, add = TRUE, col = "green")
+curve(dbeta(x/fac, zAlpha, zBeta) / fac, 0, fac, add = TRUE, col = "green")
 
 legend_labels <- c("Data", "MM", "MLE")
 legend_colors <- c("black", "red", "green")
